9089123d99f6e2a147cd45b1bb7b27f082b5eeeb
KrishnanditoLksn <krislaksana123@gmail.com>
2025-04-17 22:29:43 +0700

    init

---
+++ src/test/java/com/ditsdev/environment_simulation/EnvironmentSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ditsdev.environment_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
