7a78175d570c129062badd254dcb34284c58d59c
KrishnanditoLksn <krislaksana123@gmail.com>
2025-04-18 12:22:12 +0700

    refactor:changed from dev service impl to env service constructor params

--- src/main/java/com/ditsdev/environment_simulation/controllers/DevelopmentSimulationController.java
+++ src/main/java/com/ditsdev/environment_simulation/controllers/DevelopmentSimulationController.java
@@ -1,22 +1,23 @@
 package com.ditsdev.environment_simulation.controllers;
 
-import com.ditsdev.environment_simulation.services.implementation.DevelopmentSimulationImplementation;
+import com.ditsdev.environment_simulation.services.env.EnvironmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class DevelopmentSimulationController {
 
 
-    private final DevelopmentSimulationImplementation developmentSimulationImplementation;
+    private final EnvironmentService developmentSimulationImplementation;
 
     @Autowired
-    public DevelopmentSimulationController(DevelopmentSimulationImplementation developmentSimulationImplementation) {
+    public DevelopmentSimulationController(@Qualifier("envService") EnvironmentService developmentSimulationImplementation) {
         this.developmentSimulationImplementation = developmentSimulationImplementation;
     }
 
     public String getDevelopmentEnvironment() {
-        return developmentSimulationImplementation.getDevelopmentEnvironment();
+        return developmentSimulationImplementation.getCurrentEnvironment();
     }
 
     public boolean ensureEnvironmentsUp() {
