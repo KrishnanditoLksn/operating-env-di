8292ca0819aa3595d0c23bee6d3208695606ead1
KrishnanditoLksn <krislaksana123@gmail.com>
2025-04-18 19:57:16 +0700

    feat:set dev default profile

--- src/main/java/com/ditsdev/environment_simulation/services/dev/DevelopmentSimulationImplementation.java
+++ src/main/java/com/ditsdev/environment_simulation/services/dev/DevelopmentSimulationImplementation.java
@@ -4,7 +4,10 @@ import com.ditsdev.environment_simulation.services.env.EnvironmentService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("devProfileService")
+/**
+ * If there is no active profile , the default is devProfileService
+ */
+@Profile({"devProfileService", "default"})
 @Service("envService")
 public class DevelopmentSimulationImplementation implements EnvironmentService {
     @Override
