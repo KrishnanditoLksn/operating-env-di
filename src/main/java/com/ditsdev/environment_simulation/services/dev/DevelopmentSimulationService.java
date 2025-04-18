097a6f9b902cac5d6ba9b3dd64bcba6fe0e1d77d
KrishnanditoLksn <krislaksana123@gmail.com>
2025-04-18 12:08:37 +0700

    delete:dev and env service and env tests

--- src/main/java/com/ditsdev/environment_simulation/services/dev/DevelopmentSimulationService.java
+++
@@ -1,9 +0,0 @@
-package com.ditsdev.environment_simulation.services.dev;
-
-import com.ditsdev.environment_simulation.services.env.EnvironmentService;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface DevelopmentSimulationService extends EnvironmentService {
-    String getDevelopmentEnvironment();
-}
